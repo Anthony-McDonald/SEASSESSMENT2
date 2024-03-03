4c4f1736eb1f7bf1a27cac6ce5cd2a8fc432ac58
Tin Lok Liu <lokliu@hotmail.co.uk>
2024-03-03 14:23:03 +0000

    created basic View class, not finished

---
+++ View.java
@@ -0,0 +1,14 @@
+public class View {
+    
+    public void displayTeachingRequirements(){
+        System.out.println("Teaching Requirements: ")
+    }
+
+    public void displaySuitableTeachers(){
+        System.out.println("Suitable Teachers: ");
+    }
+
+    public void displayAllTeachers(){
+        System.out.println("List of all teachers: ");
+    }
+}
