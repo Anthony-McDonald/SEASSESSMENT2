4d8ca0ab265c5f8ab5c04fa4536e7aeafd5c6293
Tin Lok Liu <lokliu@hotmail.co.uk>
2024-03-12 13:04:17 +0000

    controller now has no more system.out.println is being handled in the view

--- View.java
+++ View.java
@@ -33,6 +33,17 @@ public class View {
         System.out.println(string);
     }
 
+    public void showProgramCommands() {
+        System.out.println("\n" + "Welcome admin! We have a list of things you could do today");
+        System.out.println("Please note that none of these commands are case or space sensitive, if you wish to:");
+        System.out.println("See teaching requirements, type: 'show teaching requirements'");
+        System.out.println("See suitable staff for a particular course, type: 'show suitable staff'");
+        System.out.println("Assign a teacher to a course, type: 'assign a teacher'");
+        System.out.println("See all assigned teaching requirements, type: 'see assigned teaching requirements'");
+        System.out.println("Edit existing teaching requirements, type: 'edit teaching requirements'");
+        System.out.println("Stop the program completely, type: 'stop'" + "\n");
+        System.out.println("If you want to see these options again, type: 'help");
+    }
 
 }
 
